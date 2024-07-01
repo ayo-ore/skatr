4fa0ad9a184bf61fee86ba03382ea0073723995f
Aaron Nordmann <ra255@stud.uni-heidelberg.de>
2024-07-01 15:41:36 +0200

    new masking (not debugged)

--- src/models/pretrainer.py
+++ src/models/pretrainer.py
@@ -74,7 +74,11 @@ class Pretrainer(Model):
             num_patches = self.student.num_patches
             match cfg.name:
                 case 'random':
-                    return masks.random_patch_mask(num_patches, cfg, batch_size, device)
+                    return masks.patch_mask(num_patches, cfg, batch_size, device)
+                case 'block':
+                    return masks.block_mask(num_patches, cfg, batch_size, device)[0]
+                case 'multi-block':
+                    return masks.context_target_mask(num_patches, cfg, batch_size, device)
 
 def augment(x, include_identity=False):
     """Applies random rotation + reflection, avoiding double counting"""
